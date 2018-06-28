dab1fee8320fe153a85a8daefb8bef5006de0fc9
Zaxner <xav4098@gmail.com>
2018-06-28 21:45:17 +0200

    Modifie the config

--- tinymvc/myapp/configs/config_global.php
+++ tinymvc/myapp/configs/config_global.php
@@ -2,8 +2,8 @@
  /* define the name of the website */
 define('NAME', 'SaberLeWeeb');
 
-/* define the url of the website (for the assets) */
-define('BASE_URL', 'http://127.0.0.1/saber/');
+/* define the url of the website (for the assets and redirection) */
+define('BASE_URL', 'http://127.0.0.1/SaberLeWeeb/');
 
 /* define if you use rewrite url or not (TRUE if you use, FALSE otherwise) */
 define('REWRITE', TRUE);
