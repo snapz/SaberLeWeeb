2e6d998d82ab63c74253a368159e4db35b5ec7c5
Zaxner <xav4098@gmail.com>
2018-06-28 21:39:35 +0200

    add rewrite

--- tinymvc/myapp/configs/config_global.php
+++ tinymvc/myapp/configs/config_global.php
@@ -5,6 +5,9 @@ define('NAME', 'SaberLeWeeb');
 /* define the url of the website (for the assets) */
 define('BASE_URL', 'http://127.0.0.1/saber/');
 
+/* define if you use rewrite url or not (TRUE if you use, FALSE otherwise) */
+define('REWRITE', TRUE);
+
 /* define the informations for MySQL */
 define('DBHOST', '127.0.0.1');
 define('DBNAME', 'saber');
